2fbd72140fb8577225ac612a87612a315fa80b8c
Ryou <22302010011@m.fudan.edu.cn>
2024-07-27 05:33:17 +0800

    1.2.1_Func: Real time updating buttons to repos

--- src.js
+++ src.js
@@ -1,14 +1,13 @@
+const username = 'RyoooooouGit';
 
 document.addEventListener('DOMContentLoaded', async (event) => {
     repoNum = await fetchRepoCount();
-    console.log(repoNum)
-    generateButtons(repoNum);
+    repoList = await fetchRepos();
+    generateButtons(repoNum, repoList);
 });
 
 async function fetchRepoCount() {
-    const username = 'RyoooooouGit';
     const url = `https://api.github.com/users/${username}`;
-
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -21,74 +20,81 @@ async function fetchRepoCount() {
     }
 }
 
-function generateButtons(number) {
+async function fetchRepos() {
+    const url = `https://api.github.com/users/${username}/repos`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+async function fetchRepoDescription(repoName) {
+    const url = `https://api.github.com/repos/${username}/${repoName}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        return data.description;
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+async function generateButton(isLeft, repoName) {
+    let div = document.createElement('div');
+    div.className = isLeft ? 'left' : 'right';
+
+    let link = document.createElement('a');
+    link.className = 'outerBackground_buttonToGithub';
+    link.href = 'https://github.com/RyoooooouGit/' + repoName;
+
+    let innerDiv = document.createElement('div');
+    innerDiv.className = 'innerBackground';
+
+    let smallerFontDiv = document.createElement('div');
+    smallerFontDiv.className = 'smallerFont';
+    smallerFontDiv.textContent = repoName;
+
+    let hr = document.createElement('hr');
+    hr.style.color = 'white';
+
+    let tinyFontDiv = document.createElement('div');
+    tinyFontDiv.className = 'tinyFont';
+    tinyFontDiv.textContent = await fetchRepoDescription(repoName);
+
+    innerDiv.appendChild(smallerFontDiv);
+    innerDiv.appendChild(hr);
+    innerDiv.appendChild(tinyFontDiv);
+    link.appendChild(innerDiv);
+    div.appendChild(link);
+
+    return div;
+}
+
+async function generateButtons(number, repoList) {
     const container = document.getElementsByClassName('containerForButtons')[0];
     let lineContainer = null;
     for (let i = 0; i < number; i++) {
+        let repoName = repoList[i].name; let buttonDiv = null;
         if (i % 2 == 0) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('containerForALine');
             container.appendChild(newDiv);
             lineContainer = newDiv;
 
-            let leftDiv = document.createElement('div');
-            leftDiv.className = 'left';
-
-            let leftLink = document.createElement('a');
-            leftLink.className = 'outerBackground_buttonToGithub';
-            leftLink.href = 'https://github.com/RyoooooouGit/Karel';
-
-            let leftInnerDiv = document.createElement('div');
-            leftInnerDiv.className = 'innerBackground';
-
-            let leftSmallerFontDiv = document.createElement('div');
-            leftSmallerFontDiv.className = 'smallerFont';
-            leftSmallerFontDiv.textContent = 'Karel';
-
-            let leftHr = document.createElement('hr');
-            leftHr.style.color = 'white';
-
-            let leftTinyFontDiv = document.createElement('div');
-            leftTinyFontDiv.className = 'tinyFont';
-            leftTinyFontDiv.textContent = '2023 | A little sokoban-like game written in java';
-
-            leftInnerDiv.appendChild(leftSmallerFontDiv);
-            leftInnerDiv.appendChild(leftHr);
-            leftInnerDiv.appendChild(leftTinyFontDiv);
-            leftLink.appendChild(leftInnerDiv);
-            leftDiv.appendChild(leftLink);
-
-            lineContainer.appendChild(leftDiv);
+            buttonDiv = await generateButton(true, repoName);
         } else {
-            let rightDiv = document.createElement('div');
-            rightDiv.className = 'right';
-
-            let rightLink = document.createElement('a');
-            rightLink.className = 'outerBackground_buttonToGithub';
-            rightLink.href = 'https://github.com/RyoooooouGit/LittleYellow';
-
-            let rightInnerDiv = document.createElement('div');
-            rightInnerDiv.className = 'innerBackground';
-
-            let rightSmallerFontDiv = document.createElement('div');
-            rightSmallerFontDiv.className = 'smallerFont';
-            rightSmallerFontDiv.textContent = 'Little Yellow';
-
-            let rightHr = document.createElement('hr');
-            rightHr.style.color = 'white';
-
-            let rightTinyFontDiv = document.createElement('div');
-            rightTinyFontDiv.className = 'tinyFont';
-            rightTinyFontDiv.textContent = '2022 | The final project with genetic algorithm of class Program Designing';
-
-            rightInnerDiv.appendChild(rightSmallerFontDiv);
-            rightInnerDiv.appendChild(rightHr);
-            rightInnerDiv.appendChild(rightTinyFontDiv);
-            rightLink.appendChild(rightInnerDiv);
-            rightDiv.appendChild(rightLink);
-
-            lineContainer.appendChild(rightDiv);
+            buttonDiv = await generateButton(false, repoName);
         }
+        lineContainer.appendChild(buttonDiv);
 
         if (i % 2 != 0 || i == number - 1) {
             container.appendChild(lineContainer);
